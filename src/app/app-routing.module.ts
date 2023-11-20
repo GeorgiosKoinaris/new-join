5cc1ef195e5f2e07371b8e8f08f755c60cc53b56
GeorgiosKoinaris <g.koinaris@gmail.com>
2023-11-20 15:09:52 +0100

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddTaskComponent } from './components/add-task/add-task.component';
+import { BoardComponent } from './components/board/board.component';
+import { SummaryComponent } from './components/summary/summary.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'summary', component: SummaryComponent },
+  { path: 'addTask', component: AddTaskComponent },
+  { path: 'board', component: BoardComponent },
+  { path: 'contacts', component: ContactsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
